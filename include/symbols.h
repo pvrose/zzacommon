9da82e0d471d4b7767a6a2eba72c9b2ccf469d46
pvrose <gm3zza@btinternet.com>
2025-12-30 09:49:25 +0000

    Prefix variable names in CMakeLists with ZZACOMMON. Adding top-level descriptions to documentation plus tweaks.

--- include/symbols.h
+++ include/symbols.h
@@ -5,12 +5,12 @@
 //! \file 
 //! This file defines extra symbols  to be used by FLTK
 
-//! Draw an open eye (eyeopen) - for password inputs indicating plain text.
+//! Draw an open eye (<B>\@eyeopen</B>) - for password inputs indicating plain text.
 void draw_eyeopen(Fl_Color c);
-//! Draw a shut eye (eyeshut) - for password inputs indicating hidden text.
+//! Draw a shut eye (<B>\@eyeshut</B>) - for password inputs indicating hidden text.
 void draw_eyeshut(Fl_Color c);
-//! Draw a calendar (calendar) - for calendar input widget.
+//! Draw a calendar (<B>\@calendar</B>) - for calendar input widget.
 void draw_calendar(Fl_Color c);
-//! Draw a letter image (mail) - as a label image for e-Mail.
+//! Draw a letter image (<B>\@mail</B>) - as a label image for e-Mail.
 void draw_mail(Fl_Color c);
 
