6297c731b5d99c976a6ee1c6ce1932a22041aba7
pvrose <gm3zza@btinternet.com>
2026-03-01 10:00:32 +0000

    Change file_holder data to reference. Fix MSVC warnings on boost::system::eror_code

--- src/zc_file_holder.cpp
+++ src/zc_file_holder.cpp
@@ -60,7 +60,6 @@ zc_file_holder::zc_file_holder(const char* arg0, const std::map<uint8_t, file_co
 	default_source_directory_ = exec_directory_ + "/";
 #endif
 
-	default_code_directory_ = default_code_directory_;
 	default_git_directory_ = APP_SOURCE_DIR + "/reference/";
 	// Test the path using the icon
 	std::string logo = get_filename(FILE_ICON_ZZA);
@@ -80,8 +79,7 @@ zc_file_holder::zc_file_holder(const char* arg0, const std::map<uint8_t, file_co
 		if (!ilog || ilog->fail()) {
 			printf("ZZALOG: Unable to find logo file - file accesses will fail\n");
 			default_source_directory_ = "";
-			default_code_directory_ = "";
-			default_data_directory_ = "";
+			default_ref_directory_ = "";
 			default_html_directory_ = "";
 		}
 	}
@@ -90,10 +88,10 @@ zc_file_holder::zc_file_holder(const char* arg0, const std::map<uint8_t, file_co
 #ifdef _WIN32
 	default_html_directory_ = default_source_directory_;
 	// Working directory
-	default_data_directory_ =
+	default_ref_directory_ =
 		std::string(getenv("APPDATA")) + "\\" + APP_VENDOR + "\\" + APP_NAME + "\\";
 	// Create the working directory
-	std::string unixified = default_data_directory_;
+	std::string unixified = default_ref_directory_;
 	for (size_t pos = 0; pos < unixified.length(); pos++) {
 		if (unixified[pos] == '\\') unixified[pos] = '/';
 	}
@@ -102,10 +100,10 @@ zc_file_holder::zc_file_holder(const char* arg0, const std::map<uint8_t, file_co
 #else 
 	default_html_directory_ = default_source_directory_;
 	// Working directory
-	default_data_directory_ =
+	default_ref_directory_ =
 		std::string(getenv("HOME")) + "/.config/" + APP_VENDOR + "/" + APP_NAME + "/";
 	// Create the working directory
-	fl_make_path(default_data_directory_.c_str());
+	fl_make_path(default_ref_directory_.c_str());
 #endif
 }
 
@@ -132,7 +130,7 @@ bool zc_file_holder::get_file(uint8_t type, std::ifstream& is, std::string& file
 			}
 		}
 		else if (DEBUG_RESET_CONFIG & ctrl.reset_mask) {
-			filename = default_data_directory_ + ctrl.filename;
+			filename = default_ref_directory_ + ctrl.filename;
 			// Copy source to working
 			if (copy_source_to_working(type)) {
 				is.open(filename);
@@ -152,7 +150,7 @@ bool zc_file_holder::get_file(uint8_t type, std::ifstream& is, std::string& file
 			}
 		}
 		else {
-			filename = default_data_directory_ + ctrl.filename;
+			filename = default_ref_directory_ + ctrl.filename;
 			is.open(filename);
 			if (is.fail()) {
 				snprintf(msg, sizeof(msg), "FILE: Cannot open %s", filename.c_str());
@@ -186,7 +184,7 @@ bool zc_file_holder::get_file(uint8_t type, std::ifstream& is, std::string& file
 		}
 	}
 	else {
-		filename = default_data_directory_ + ctrl.filename;
+		filename = default_ref_directory_ + ctrl.filename;
 		if (DEBUG_RESET_CONFIG & ctrl.reset_mask) {
 			DEBUG_RESET_CONFIG &= ~(ctrl.reset_mask);
 			snprintf(msg, sizeof(msg), "FILE: Ignoring %s", filename.c_str());
@@ -216,7 +214,7 @@ bool zc_file_holder::copy_source_to_working(uint8_t type) {
 	file_control_t ctrl = control_data_.at(type);
 	// Copy source to working
 	std::string source = default_source_directory_ + ctrl.filename;
-	std::string filename = default_data_directory_ + ctrl.filename;
+	std::string filename = default_ref_directory_ + ctrl.filename;
 // #ifdef _WIN32
 // 	std::string command = "copy " + source + " " + filename;
 // #else
@@ -238,7 +236,10 @@ bool zc_file_holder::copy_source_to_working(uint8_t type) {
 	);
 	char msg[256];
 	if (ec) {
-		snprintf(msg, sizeof(msg), "FILE: Copy %s to %s failed %d", source.c_str(), filename.c_str(), ec);
+		snprintf(msg, sizeof(msg), "FILE: Copy %s to %s failed %s", 
+			source.c_str(), 
+			filename.c_str(), 
+			ec.message().c_str());
 		if (status_) status_->misc_status(ST_ERROR, msg);
 		return false;
 	}
@@ -259,7 +260,7 @@ bool zc_file_holder::get_file(uint8_t type, std::ofstream& os, std::string& file
 		if (status_) status_->misc_status(ST_ERROR, msg);
 		return false;
 	}
-	filename = default_data_directory_ + ctrl.filename;
+	filename = default_ref_directory_ + ctrl.filename;
 	os.open(filename);
 	if (os.fail()) {
 		snprintf(msg, sizeof(msg), "FILE: Cannot open %s", filename.c_str());
@@ -285,7 +286,7 @@ bool zc_file_holder::copy_source_to_git(uint8_t type) const {
 	);
 	char msg[256];
 	if (ec) {
-		snprintf(msg, sizeof(msg), "FILE: Copy failed %d", ec);
+		snprintf(msg, sizeof(msg), "FILE: Copy failed %s", ec.message().c_str());
 		if (status_) status_->misc_status(ST_ERROR, msg);
 		return false;
 	}
@@ -302,7 +303,7 @@ bool zc_file_holder::copy_working_to_source(uint8_t type) const {
 	file_control_t ctrl = file_holder_->file_control(type);
 	// Copy source to working
 	std::string source = default_source_directory_ + ctrl.filename;
-	std::string working = default_data_directory_ + ctrl.filename;
+	std::string working = default_ref_directory_ + ctrl.filename;
 	boost::system::error_code ec;
 	boost::filesystem::copy(
 		working.c_str(),
@@ -313,7 +314,7 @@ bool zc_file_holder::copy_working_to_source(uint8_t type) const {
 	);
 	char msg[256];
 	if (ec) {
-		snprintf(msg, sizeof(msg), "FILE: Copy failed %d", ec);
+		snprintf(msg, sizeof(msg), "FILE: Copy failed %s", ec.message().c_str());
 		if (status_) status_->misc_status(ST_ERROR, msg);
 		return false;
 	}
@@ -333,8 +334,8 @@ file_control_t zc_file_holder::file_control(uint8_t type) const {
 void zc_file_holder::display_info() const {
 	if (status_) {
 		status_->misc_status(ST_NOTE, "FILE: Binary: %s", exec_directory_.c_str());
-		status_->misc_status(ST_NOTE, "FILE: Source data:- %s", default_source_directory_.c_str());
-		status_->misc_status(ST_NOTE, "FILE: Working data:- %s", default_data_directory_.c_str());
+		status_->misc_status(ST_NOTE, "FILE: Source reference:- %s", default_source_directory_.c_str());
+		status_->misc_status(ST_NOTE, "FILE: Working reference:- %s", default_ref_directory_.c_str());
 	}
 }
 
