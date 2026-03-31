0b6a488f45691a1c2390149018df3f480ff0960e
pvrose <gm3zza@btinternet.com>
2026-03-31 16:47:06 +0100

    Remove platform specific code

--- src/zc_serial.cpp
+++ src/zc_serial.cpp
@@ -21,18 +21,6 @@
 #include <set>
 #include <string>
 
-//#ifdef _WIN32
-//// Note the code is Windows only - need Linux version
-//#include <Windows.h>
-//#include <sys/stat.h>
-//#else 
-//#include <errno.h>
-//#include <fcntl.h> 
-//#include <termios.h>
-//#include <unistd.h>
-//#include <sys/stat.h>
-//#endif
-
 //! Include boost asio for cross-platform serial port access.
 #include <boost/asio.hpp>
 
@@ -86,60 +74,6 @@ bool zc_serial::available_ports(int num_ports, std::string* ports, bool all_port
 		}
 	}
 
-//#ifdef _WIN32
-//	const unsigned int PATH_MAX = 255;
-//	// Find which ports exists (not just available) by trying to open the port
-//	for (unsigned int i = 0; i < MAX_TTY; i++) {
-//		char dev[16];
-//		bool use_port = false;
-//		snprintf(dev, sizeof(dev), "//./COM%u", i);
-//		HANDLE fd = CreateFile(dev, GENERIC_READ | GENERIC_WRITE, 0, 0, OPEN_EXISTING, 0, 0);
-//		if (fd != INVALID_HANDLE_VALUE) {
-//			// Accessed OK - so exists and available for use
-//			CloseHandle(fd);
-//			use_port = true;
-//		}
-//		else {
-//			// Check if the port was there but access denied - implies exists but in use
-//			long error_code = GetLastError();
-//			if (error_code == ERROR_ACCESS_DENIED && all_ports) {
-//				use_port = true;
-//			}
-//		}
-//		// Add it to the list of ports
-//		if (use_port) {
-//			if (actual_ports < num_ports) {
-//				char port[16];
-//				snprintf(port, 16, "COM%d", i);
-//				ports[actual_ports] = port;
-//			}
-//			actual_ports++;
-//		}
-//	}
-//#else
-//	struct stat st {};
-//	char ttyname[PATH_MAX + 1];
-//	// Currently limited to USB TTY ports
-//	const char* tty_fmt[] = {
-////		"/dev/tty%u",
-////		"/dev/ttyS%u",
-//		"/dev/ttyUSB%u",
-//		"/dev/ttyACM%u" //,
-////		"/dev/usb/ttyUSB%u"
-//	};
-//	for (size_t i = 0; i < sizeof(tty_fmt)/sizeof(*tty_fmt); i++) {
-//		for (unsigned j = 0; j < MAX_TTY; j++) {
-//			snprintf(ttyname, sizeof(ttyname), tty_fmt[i], j);
-//			if ( !(stat(ttyname, &st) == 0 && S_ISCHR(st.st_mode)) )
-//				continue;
-//// TODO check whether port is available 
-//			if (actual_ports < num_ports) {
-//				ports[actual_ports] = ttyname;
-//			} 
-//			actual_ports++;
-//		}
-//	}
-//#endif
 	return (actual_ports <= num_ports);
 
 }
